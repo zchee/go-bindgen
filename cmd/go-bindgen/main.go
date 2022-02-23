59c2ceee3f8fe1e258601653491cdaede701c4aa
Koichi Shiraishi <zchee.io@gmail.com>
2022-02-24 06:11:05 +0900

    cmd/go-bindgen: add go-bindgen command boilerplate
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ cmd/go-bindgen/main.go
@@ -0,0 +1,5 @@
+// Copyright 2022 The go-bindgen Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Command go-bindgen automatically generates Go cgo bindings for C (and some C++) libraries.
+package main
